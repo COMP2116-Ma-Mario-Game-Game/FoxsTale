1ee83f0580bc97f17f04e58f6e4feb9064a49a6c
Astro5616 <Csh644100Csh@outlook.com>
2025-04-03 12:36:30 +0800

    init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Transform farBackground, middleBackground; //我们希望让背景随着角色移动而移动
+
+    public float minHeight, maxHeight; //我们希望相机可拍摄有限高度，而不希望其跌入谷底或升至天边
+
+    //private float lastXPos;
+    private Vector2 lastPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //lastXPos = transform.position.x;
+        lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() //移动相机的位置，以便链接相机到角色
+    {
+        /* transform.position = new Vector3(target.position.x, target.position.y, transform.position.z); //实际上用不到z,我们也不希望摄像机随着小狐狸跳跃而跳跃。
+
+
+        float clampedYPos = Mathf.Clamp(transform.position.y, minHeight, maxHeight); //限制相机的高度,Clamp是一个函数，用于限制数值的范围 */
+        transform.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z);
+
+        //float amountToMoveX = transform.position.x - lastXPos; //在x轴上移动的距离
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPos.x, transform.position.y - lastPos.y); //在x轴和y轴上移动的距离
+        farBackground.position = farBackground.position + new Vector3(amountToMove.x, amountToMove.y, 0f); //记住不可以直接使用camera的position，因为如果z轴相同，camera将没有能力拍到他们
+        middleBackground.position = middleBackground.position + new Vector3(amountToMove.x, amountToMove.y, 0f) * 0.5f; //海与灌木不同的速度，让世界更有深度
+
+        //lastXPos = transform.position.x; //每次移动都更新lastPos
+        lastPos = transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using System.Collections; //任何计算机都会使用的基本元素，如数字字母字符串
+using System.Collections.Generic;
+using UnityEngine; //Unity引擎，允许我们使用Unity某些内置元素（physics/colliders/transform）
+
+public class PlayerController : MonoBehaviour //MonoBehaviour是一个普通脚本
+{
+    public float moveSpeed; //pubic/private显示他是否可以在编译器中被看到
+    public Rigidbody2D theRB; //控制角色物理的组件 刚体
+    public float jumpForce;
+
+    private bool isGrounded; //判断是否在地面上(ground),默认为false
+    public Transform groundCheckPoint; //用来禁止空中无限跳跃
+    public LayerMask whatIsGround;
+
+    private bool canDoubleJump; //双跳
+
+    private Animator anim; //动画控制
+    private SpriteRenderer theSR; //左右翻转
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() //游戏开始立即运行
+    {
+        anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() //frames 视觉发生，角色移动，每秒发生的改变
+    {
+        theRB.linearVelocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), theRB.linearVelocity.y); //GetAxisRaw获取-1到1的值(用户直接输入)，而GetAxis在ProjectSettings里面的InputManager内是设置为3的，所以看起来滑溜溜的。而我不想要这种质感。
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, 0.2f, whatIsGround);
+
+        if (isGrounded) //如果现在在地面上，则下一次跳跃应该被允许双跳
+        {
+            canDoubleJump = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                theRB.linearVelocity = new Vector2(theRB.linearVelocity.x, jumpForce);
+            }
+            else //如果此刻在空中
+            {
+                if (canDoubleJump) //在空中且在上一阶段（没有经历过双跳/没被设为false）
+                {
+                    theRB.linearVelocity = new Vector2(theRB.linearVelocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+        }
+
+        if (theRB.linearVelocity.x < 0)
+        {
+            theSR.flipX = true; //左移
+        }
+        else if (theRB.linearVelocity.x > 0) //这里是右移，但如果x=0的话还是默认为刚刚那个状态
+        {
+            theSR.flipX = false;
+        }
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("moveSpeed", Mathf.Abs(theRB.linearVelocity.x)); //这里要设计绝对值，因为左移是负数体现
+
+    }
+}
